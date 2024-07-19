3e90f1418e3b2f46907eda703d0274a493db99f1
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-07-19 13:03:40 -0300

    Initializing the project

---
+++ src/main/java/com/akross/akrossapi/AkrossapiApplication.java
@@ -0,0 +1,13 @@
+package com.akross.akrossapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkrossapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkrossapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akross/akrossapi/AkrossapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akross.akrossapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkrossapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
